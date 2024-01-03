e8a3683e3e9dfabb9f9a1c8a4cd6049fefc1a3d5
Isaac Coleman <isaaccoleman58@gmail.com>
2024-01-03 20:46:55 +0000

    adding current files / first commit

---
+++ creating_scrabble_dictionary.py
@@ -0,0 +1,66 @@
+import os
+import csv
+
+# scrabble score system in a python dictionary
+score_system = {'a':1, 'b':3, 'c':3, 'd':2,'e':1, 'f':4, 'g':2,'h':4,'i':1, 'j':8, 'k':5, 'l':1, 'm':3, 'n':1, 'o':1, 'p':3, 'q':10, 'r':1, 's':1, 't':1, 'u':1, 'v':4, 'w':4, 'x':8, 'y':4, 'z':10}
+
+def word_score(word):
+    ''' This function calculates the scrabble word score of a given word (excluding any extra positional points e.g. triple word score)'''
+
+    score = 0
+    for letter in word.lower():
+        try:
+            score += score_system[letter]
+        except:
+            #removing any words with spaces, hyphens or other non-alphabetical characters
+            continue
+    
+    return score
+
+
+# creating system agnostic paths for the current folder and the 'word list in csv' folder within
+current_folder = os.path.dirname(os.path.abspath(__file__))
+words_folder = os.path.join(current_folder, 'word lists in csv')
+
+# constructing a list of the file names in the word list folder (alphabetical order)
+words_file_list = sorted(os.listdir(words_folder))
+
+# creating a list which will accumulate the words across all of the (26) csv files
+all_words_list = []
+
+# this for loop iterates through each csv file in the 'word lists in csv' folder, adding each line of the files to the all_words_list list
+for file in words_file_list:
+    file_path = os.path.join(words_folder, file)
+    with open(file_path, 'r', encoding='iso-8859-1') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row: # skips any empty rows
+                word = row[0].strip()
+                all_words_list.append(word)
+
+# removing any repeat words
+all_unique_words = sorted(list(set(all_words_list)))
+
+# removing all errors in the given csv files of words. This includes multiple words, hyphenated words, and other non-alphabetic characters
+all_scrabble_words = [word for word in all_unique_words if all(char.isalpha() for char in word)]
+
+print(len(all_unique_words))
+print(len(all_scrabble_words))
+
+
+with open('scrabble_dictionary.csv', 'w') as file:
+    writer_object = csv.writer(file)
+    for word in all_scrabble_words:
+        writer_object.writerow([word,word_score(word)])
+
+
+
+
+print('done')
+
+
+''' NOW NEED TO ADD WORDS FROM LIST INTO A CSV FILE'''
+''' THEN NEED TO CACLCULATE THE POINTS FOR EACH WORD -  DO THAT BY CREATING A FUNCTION AND THEN DOING IT SIMULTANEOUSLY WITH ABOVE STEP!!'''
+
+''' THEN CAN WORK ON SCRIPT'''
+'''FOLLOWED BY THE GUI'''
---
+++ script.py
@@ -0,0 +1,78 @@
+import os 
+import csv
+
+#user_input = input('Please input your set of letters (no separations)')
+
+
+''' Each line of the csv file has two elements: the first is a word, and the second is its scrabble word score.
+    To make the access of word scores easier, below I'll make a python dictionary with the keys as words, and their 
+    correspodning values as the word scores. '''
+scrabble_dictionary = {}
+
+current_folder = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(current_folder, 'scrabble_dictionary.csv'), 'r') as file:
+    reader_obj = csv.reader(file)
+
+    for row in reader_obj:
+        key,value = row
+        scrabble_dictionary[key] = int(value)
+
+
+
+def available_words(list_of_letters):
+    ''' This function accepts a list (or any other iterable) of letters, and returns a list with all of 
+        the possible words formed from those letters. '''
+    
+    final_list = []
+
+    list = [word for word in scrabble_dictionary.keys()]
+    
+    # This for loop updates the final_list list, which aims to consist of words only with the inputted letters
+    # The longest word in the dictionary is 21 letters. Hence, numbers 0-20 are needed to index every possible word
+    for i in range(21):
+        list = [word for word in list if word[i] in list_of_letters]
+        final_list += [word for word in list if len(word) == i+1]
+        list = [word for word in list if len(word) != i+1]
+
+
+    # The following code removes words from the final_list list that have several occurences of the same letter which the user's
+    # list doesn't have
+
+    final_list = [word for word in final_list if len(word) <= len(list_of_letters)]
+
+    # dictionary with keys as unique letters in inputted list, and values as the count in the list
+    letter_count = {}
+
+    for letter in list_of_letters:
+        letter_count[letter] = letter_count.get(letter, 0) + 1
+
+    for key, value in letter_count.items():
+        final_list = [word for word in final_list if word.count(key) <= value]
+                
+
+    return final_list
+
+
+def scrabble_cheat_list(list_of_letters):
+    ''' This function accepts a list (or any other iterable) of letters and returns a list of tuples 
+        with the first elements being all of the possible words formed from those letters, and the second element
+        being the corresponding scrabble word score.'''
+    
+    list_of_words = available_words(list_of_letters)
+    return sorted([(word, scrabble_dictionary[word]) for word in list_of_words], key = lambda i:i[1], reverse=True)
+
+
+
+#print(scrabble_cheat_list(['a','o', 'e', 't', 's', 'v', 'b']))
+#print('\n'.join(scrabble_cheat_list(['a','o', 'e', 't', 's', 'v', 'b'])))
+
+
+a = scrabble_cheat_list(['e', 'a', 's', 'p', 't', 'i', 'h', 'l'])
+b = [f'{tupl[0]}: {tupl[1]}' for tupl in a]
+
+print(b)
+
+
+''' CREATE THE UI NOWWWWWWWWWWW'''
+
